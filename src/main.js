bf56c53596eae4f0c6acd76a062f4d5dfa57f3ae
RazraB0tchik <100565729+RazraB0tchik@users.noreply.github.com>
2022-10-25 16:00:52 +1000

    Comit_One

--- src/main.js
+++ src/main.js
@@ -1,4 +1,41 @@
-import { createApp } from 'vue'
+import {createApp}from 'vue'
 import App from './App.vue'
+import router from './routs/router'
+const app =createApp(App)
+    // el: '#app',
+    // data(){
+    //     return{
+    //         form: {}
+    //     }
+    // },
+//     methods: {
+//         sendForm(e) {
+//             e.preventDefault()
+//             console.log("Отправка JSON", this.form)
+//
+//             fetch(
+//                 e.target.action,
+//                 {
+//                     method: 'POST',
+//                     mode: 'cors',
+//                     cache: 'no-cache',
+//                     credentials: 'same-origin',
+//                     headers: {
+//                         'Content-Type': 'application/json'
+//                     },
+//                     body: JSON.stringify(this.form)
+//                 }
+//             ).then(
+//                 function (response) {
+//                     console.log("Ответ сервера", response);
+//                 },
+//                 function (error) {
+//                     console.log(error);
+//                 }
+//             );
+//         }
+//         },
+// })
+app.use(router); //регистрируем роутер
+app.mount('#app') //загружает компонент
 
-createApp(App).mount('#app')
